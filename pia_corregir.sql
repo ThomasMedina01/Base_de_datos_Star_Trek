e179888ed6ca999b17dc14d7d185a3a7e9817405
diegoalz <diego.lunaza@uanl.edu.mx>
2020-05-18 17:59:05 -0500

    tablas relacionadas

--- pia_corregir.sql
+++ pia_corregir.sql
@@ -1,22 +1,3 @@
-create table Imperio(
-id_galactico int primary key not null,
-nombre_imperio varchar(25),
-temperatura_promedio varchar(15))
-
-create table Planeta(
-nombre_cientifico varchar(10) primary key not null,
-nombre_comun varchar(10),
-poblacion_total int,
-coordenada_galactica varchar(50))
-
-create table Flota(
-id_flota int primary key not null,
-nombre_flota varchar(20))
-
-create table Destino(
-id_destino int primary key not null,
-nombre_destino varchar(20))
-
 create table Capitan(
 id_capitan int primary key not null,
 nombre_capitan varchar(30))
@@ -25,7 +6,18 @@ create table Nave(
 id_nave int primary key not null,
 modelo varchar(15),
 vel_max varchar(20),
-energia_acumulada varchar(20))
+energia_acumulada varchar(20),
+id_capitan int foreign key references Capitan(id_capitan))
+
+create table Flota(
+id_flota int primary key not null,
+nombre_flota varchar(20),
+id_nave int foreign key references Nave(id_nave))
+
+create table Destino(
+id_destino int primary key not null,
+nombre_destino varchar(20),
+id_flota int foreign key references Flota(id_flota))
 
 create table Mision(
 id_mision int primary key not null,
@@ -33,7 +25,7 @@ nombre_mision varchar(15),
 descripcion varchar(30))
 
 create table Raza(
-nombre_cientifico_raza int primary key not null)
+nombre_cientifico_raza varchar(10) primary key not null)
 
 create table Maniobra(
 id_maniobra int primary key not null,
@@ -51,13 +43,43 @@ id_montana int primary key not null,
 nombre_montana varchar(10),
 altura varchar(50))
 
-create table Nave_maniobra()
+create table Planeta(
+nombre_cientifico varchar(10) primary key not null,
+nombre_comun varchar(10),
+poblacion_total int,
+coordenada_galactica varchar(50),
+id_destino int foreign key references Destino(id_destino),
+id_capitan int foreign key references Capitan(id_capitan),
+id_montana int foreign key references Montana(id_montana))
+
+create table Imperio(
+id_galactico int primary key not null,
+nombre_imperio varchar(25),
+temperatura_promedio varchar(15),
+id_flota int foreign key references Flota(id_flota),
+id_destino int foreign key references Destino(id_destino),
+id_capitan int foreign key references Capitan(id_capitan),
+planeta varchar(10) foreign key references Planeta(nombre_cientifico))
+
+create table Nave_maniobra(
+id_nave int foreign key references Nave(id_nave),
+id_maniobra int foreign key references Maniobra(id_maniobra),
+primary key(id_nave, id_maniobra))
 
 create table Poblacion(
-porcentaje_poblacion int)
+nombre_cientifico varchar(10) foreign key references Planeta(nombre_cientifico),
+nombre_cientifico_raza varchar(10) foreign key references Raza(nombre_cientifico_raza),
+porcentaje_poblacion int,
+primary key(nombre_cientifico, nombre_cientifico_raza))
 
-create table Raza_habilidad()
+create table Raza_habilidad(
+nombre_cientifico_raza varchar(10) foreign key references Raza(nombre_cientifico_raza),
+id_habilidad int foreign key references Habilidad(id_habilidad)
+primary key(nombre_cientifico_raza, id_habilidad))
 
 create table Fecha_mision(
+id_flota int foreign key references Flota(id_flota),
+id_mision int foreign key references Mision(id_mision),
 fecha_inicio datetime,
-fecha_fin datetime)
+fecha_fin datetime,
+primary key(id_flota, id_mision))
